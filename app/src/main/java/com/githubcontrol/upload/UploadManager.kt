a1dd339888c142f2cc06deb907e3f656d1a9608f
sac-ctrl <sacan316@gmail.com>
2026-04-25 20:52:13 +0000

    Fix missing closing brace in runReplaceFolder function

--- app/src/main/java/com/githubcontrol/upload/UploadManager.kt
+++ app/src/main/java/com/githubcontrol/upload/UploadManager.kt
@@ -324,6 +324,7 @@ class UploadManager @Inject constructor(
         // Always use JGit for REPLACE_FOLDER to avoid API 422 errors
         runReplaceFolderWithJGit(job)
     }
+    }
 
     private suspend fun runReplaceFolderWithJGit(job: UploadJob) = withContext(Dispatchers.IO) {
         try {
